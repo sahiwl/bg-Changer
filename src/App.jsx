51e899ac78357309e133ab4e61a126bd8b032cc2
Sahil Kumar Ray <piyushkrray1@gmail.com>
2024-02-16 10:12:57 +0530

    added divs

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,15 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div className="bg-yellow-300">let's get going</div>
-    </>
+   <div className="h-screen w-screen bg-green-200">
+      <div className="bg-blue-400 flex justify-center">outside div
+        <div className="bg-yellow-200 h-40 w-72">another div
+          <button className='bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded' onClick={document.body.style="background: red"}>red</button>
+
+        </div>
+      </div>
+
+   </div>
   )
 }
 
