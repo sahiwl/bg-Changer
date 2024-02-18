74b011e9f54412a4acd8cc128bf62b369a30733f
Sahil Kumar Ray <piyushkrray1@gmail.com>
2024-02-18 16:09:58 +0530

    final changes

--- src/App.jsx
+++ src/App.jsx
@@ -7,27 +7,30 @@ function App() {
   const [count, setCount] = useState("")
 
   return (
-   <div className="h-screen w-screen b-green-200">
-      <div className="bg-blue-400 flex justify-center">outside div
-        <div className="bg-yellow-200 h-40 w-100 absolute bottom-0">another div
-          <button className='bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded' onClick={()=>{document.body.style="background-color: red"}}>Red</button>
+    <div className="h-screen w-screen flex justify-center">
+      <div className="absolute bottom-14 grid rows-2">
+        
+        <div className="p-5 h-20  bg-slate-100 rounded shadow-2xl " style={{width: "80rem"}}>
+          <div className="flex justify-around">
+            <button className='bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded' onClick={()=>{document.body.style="background-color: red"}}>Red</button>
 
-          <button className='bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded' onClick={()=>{document.body.style="background-color: yellow"}}>Yellow</button>
+            <button className='bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded' onClick={()=>{document.body.style="background-color: yellow"}}>Yellow</button>
           
-          <button className='bg-zinc-500 hover:bg-zinc-700 text-white font-bold py-2 px-4 rounded' onClick={()=>{document.body.style="background-color: black"}}>Black</button>
+            <button className='bg-zinc-800 hover:bg-zinc-900 text-white font-bold py-2 px-4 rounded' onClick={()=>{document.body.style="background-color: black"}}>Black</button>
 
-          <button className='bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded' onClick={()=>{document.body.style="background-color: purple"}}>Purple</button>
+            <button className='bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded' onClick={()=>{document.body.style="background-color: purple"}}>Purple</button>
 
-          <button className='bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded' onClick={()=>{document.body.style="background-color: green"}}>Green</button>
+            <button className='bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded' onClick={()=>{document.body.style="background-color: green"}}>Green</button>
 
-          <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded' onClick={()=>{document.body.style="background-color: blue"}}>Blue</button>
+            <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded' onClick={()=>{document.body.style="background-color: blue"}}>Blue</button>
 
-          <button className='bg-red-300 hover:bg-red-400 text-white font-bold py-2 px-4 rounded' onClick={()=>{document.body.style="background-color: white"}}>Default</button>
-
+            <button className='bg-red-300 hover:bg-red-400 text-white font-bold py-2 px-4 rounded' onClick={()=>{document.body.style="background-color: white"}}>Default</button>
+          </div>
         </div>
+        <div className="mt-10 text-center"> <p className='text-gray-500'>made by <a href="https://github.com/sahiwl" className='font-medium text-blue-600 underline'>sahil</a></p></div>
       </div>
-
-   </div>
+      
+   </div> 
   )
 }
 
